45d694d2504ace955a94fdc1149911c633552d5d
musicode <musicode@qq.com>
2017-01-21 14:01:58 +0800

    update

--- src/platform/web/oldApi.js
+++ src/platform/web/oldApi.js
@@ -144,8 +144,8 @@ function removeChangeListener(element, listener) {
 
 function isBox(element) {
   return element.tagName === 'INPUT'
-  || element.type === 'radio'
-  || element.type === 'checkbox'
+    || element.type === 'radio'
+    || element.type === 'checkbox'
 }
 
 export function on(element, type, listener) {
