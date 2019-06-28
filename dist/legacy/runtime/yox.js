5e85f095745e90403f997d17e68843531009f6a0
musicode <musicode@qq.com>
2019-06-28 22:55:47 +0800

    1.0.0-alpha.83 类型写错了...

--- dist/legacy/runtime/yox.js
+++ dist/legacy/runtime/yox.js
@@ -1,5 +1,5 @@
 /**
- * yox.js v1.0.0-alpha.82
+ * yox.js v1.0.0-alpha.83
  * (c) 2017-2019 musicode
  * Released under the MIT License.
  */
@@ -4182,7 +4182,7 @@
       /**
        * core 版本
        */
-      Yox.version = "1.0.0-alpha.82";
+      Yox.version = "1.0.0-alpha.83";
       /**
        * 方便外部共用的通用逻辑，特别是写插件，减少重复代码
        */
--- dist/yox.d.ts
+++ dist/yox.d.ts
@@ -72,7 +72,7 @@ export interface YoxInterface {
 	nextTick(task: Function): void;
 	toggle(keypath: string): boolean;
 	increase(keypath: string, step?: number, max?: number): number | void;
-	decrease(keypath: string, step: number, min?: number): number | void;
+	decrease(keypath: string, step?: number, min?: number): number | void;
 	insert(keypath: string, item: any, index: number | boolean): true | void;
 	append(keypath: string, item: any): true | void;
 	prepend(keypath: string, item: any): true | void;
